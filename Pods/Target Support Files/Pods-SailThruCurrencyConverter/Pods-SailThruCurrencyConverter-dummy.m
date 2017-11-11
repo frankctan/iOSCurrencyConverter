1b4d1dfe1cc6923303143ad6d350f006df9fe274
Frank Tan <frankctan@gmail.com>
2017-11-11 13:04:20 -0500

    Add gitignore and cocoapods

---
+++ Pods/Target Support Files/Pods-SailThruCurrencyConverter/Pods-SailThruCurrencyConverter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SailThruCurrencyConverter : NSObject
+@end
+@implementation PodsDummy_Pods_SailThruCurrencyConverter
+@end
---
+++ Pods/Target Support Files/Pods-SailThruCurrencyConverter/Pods-SailThruCurrencyConverter-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SailThruCurrencyConverterVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SailThruCurrencyConverterVersionString[];
+
